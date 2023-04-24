154f39d45a7ded3b98e7d1456219cf398a5fdae2
Pavlo Lysiuk <pavel.lysiuk@gmail.com>
2023-04-24 20:00:55 +0300

    fixed gototop button js

--- src/js/gotop.js
+++ src/js/gotop.js
@@ -1,16 +1,25 @@
-const gotopWrapper = document.querySelector('.gotop-wrapper');
+// Отримуємо елемент "gotop-wrapper"
+const goToTopWrapper = document.querySelector('.gotop-wrapper');
 
-window.addEventListener('scroll', function () {
-  if (window.pageYOffset > 880) {
-    gotopWrapper.classList.add('show');
+// Отримуємо кнопку "Go to top"
+const goToTopBtn = document.querySelector('.gototop');
+
+// Додаємо прослуховувач подій для визначення положення користувача на сторінці
+window.addEventListener('scroll', () => {
+  // Визначаємо положення користувача на сторінці
+  const currentPosition =
+    window.pageYOffset || document.documentElement.scrollTop;
+
+  // Визначаємо висоту вмісту сторінки
+  const pageHeight = document.documentElement.scrollHeight;
+
+  // Визначаємо висоту вікна браузера
+  const windowHeight = window.innerHeight;
+
+  // Якщо користувач дійшов до певної відстані від кінця сторінки, показуємо елемент "gotop-wrapper"
+  if (currentPosition + windowHeight >= pageHeight - 140) {
+    goToTopWrapper.classList.add('show');
   } else {
-    gotopWrapper.classList.remove('show');
+    goToTopWrapper.classList.remove('show');
   }
-});
-
-gotopWrapper.addEventListener('click', function () {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth',
-  });
 });
